e84975fd725334b47f327b55d1dff04b592f035a
Jina Zhu <jinazhu87@gmail.com>
2020-04-17 18:37:54 -0700

    created search bar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const APP_ID = "9a33db56";
+  const APP_KEY = "20afb677ae19a5226f7c2c88e8413be1";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
