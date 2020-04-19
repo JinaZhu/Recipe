622b7bf95f6deadcee86aa5cee805a0aa7ec08cc
Jina Zhu <jinazhu87@gmail.com>
2020-04-19 12:30:12 -0700

    added recipe style page

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,10 +1,11 @@
 import React from "react";
+import style from "./";
 
 const Recipes = ({ title, calories, image, ingredients }) => {
   console.log(title);
   return (
     <div>
-      <h1>{title}</h1>
+      <h1 clasName={style.recipe}>{title}</h1>
       <ol>
         {ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
